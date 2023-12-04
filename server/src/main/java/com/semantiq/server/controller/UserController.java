7b315ae6a16ad036b063dc314b411bc1aa42ad87
kerem <keremozrtk@gmail.com>
2023-12-04 23:59:51 +0300

    Started User Controller as an example.

--- server/src/main/java/com/semantiq/server/controller/UserController.java
+++ server/src/main/java/com/semantiq/server/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.semantiq.server.controller;
 
+import com.semantiq.server.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService UserService;
+
+    @Autowired
+    public  UserController(UserService UserService) {
+        this.UserService = UserService;
+    }
+
+    @PostMapping("/{email}")
+    public ResponseEntity<?> login(@PathVariable String email, @RequestBody String password) {
+
+    }
 }
