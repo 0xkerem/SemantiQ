126bd5e1d16ef13eecfecd24cf0b0dc36dd71fc1
ibrahim <AlperOluc@outlook.com>
2023-12-08 18:38:11 +0300

    BotData-Attiributes and GettersSetters

--- server/src/main/java/com/semantiq/server/entity/BotData.java
+++ server/src/main/java/com/semantiq/server/entity/BotData.java
@@ -1,4 +1,49 @@
 package com.semantiq.server.entity;
 
 public class BotData {
+    int id;
+    ChatBot bot;
+    int CountPos = 0;
+    int CountNeg = 0;
+    String formData;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ChatBot getBot() {
+        return bot;
+    }
+
+    public void setBot(ChatBot bot) {
+        this.bot = bot;
+    }
+
+    public int getCountPos() {
+        return CountPos;
+    }
+
+    public void setCountPos(int countPos) {
+        CountPos = countPos;
+    }
+
+    public int getCountNeg() {
+        return CountNeg;
+    }
+
+    public void setCountNeg(int countNeg) {
+        CountNeg = countNeg;
+    }
+
+    public String getFormData() {
+        return formData;
+    }
+
+    public void setFormData(String formData) {
+        this.formData = formData;
+    }
 }
