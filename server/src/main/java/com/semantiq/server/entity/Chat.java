cd733ed29bdf1b9e68663c38304f790b70b95df6
ibrahim <AlperOluc@outlook.com>
2023-12-08 19:02:45 +0300

    Chat-Attiributes and GettersSetters

--- server/src/main/java/com/semantiq/server/entity/Chat.java
+++ server/src/main/java/com/semantiq/server/entity/Chat.java
@@ -1,4 +1,31 @@
 package com.semantiq.server.entity;
 
 public class Chat {
+    int id;
+    ChatBot bot;
+    String chatHistory;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ChatBot getBot() {
+        return bot;
+    }
+
+    public void setBot(ChatBot bot) {
+        this.bot = bot;
+    }
+
+    public String getChatHistory() {
+        return chatHistory;
+    }
+
+    public void setChatHistory(String chatHistory) {
+        this.chatHistory = chatHistory;
+    }
 }
