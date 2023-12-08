1de509a505f2550374c2949284f840c446cb1007
ibrahim <AlperOluc@outlook.com>
2023-12-08 19:10:51 +0300

    ChatBot-Attiributes and GettersSetters

--- server/src/main/java/com/semantiq/server/entity/ChatBot.java
+++ server/src/main/java/com/semantiq/server/entity/ChatBot.java
@@ -1,4 +1,49 @@
 package com.semantiq.server.entity;
 
 public class ChatBot {
+    int id;
+    String botName;
+    User owner;
+    BotData data;
+    Chat[] chatList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public BotData getData() {
+        return data;
+    }
+
+    public void setData(BotData data) {
+        this.data = data;
+    }
+
+    public Chat[] getChatList() {
+        return chatList;
+    }
+
+    public void setChatList(Chat[] chatList) {
+        this.chatList = chatList;
+    }
 }
