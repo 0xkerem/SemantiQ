3aeef02d556a8de08fecfa45e4228f546df86709
kerem <keremozrtk@gmail.com>
2023-12-19 22:40:03 +0300

    login event has been created.

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -4,7 +4,7 @@ import Signup from './Signup'; // Import your Signup component here
 export default function Login() {
   const [showSignup, setShowSignup] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
-  
+
   useEffect(() => {
     const rememberMeValue = localStorage.getItem('rememberMe');
     if (rememberMeValue && rememberMeValue === 'true') {
@@ -26,6 +26,44 @@ export default function Login() {
     localStorage.setItem('rememberMe', checked ? 'true' : 'false');
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    event.target.email.value = '';
+    event.target.password.value = '';
+
+    try {
+      const response = await fetch('http://localhost:8080/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (response.status === 401) {
+        // Handle unauthorized access error
+        alert('Invalid email or password');
+        return;
+      } else if (response.status === 403) {
+        alert('User not verified! Please verify your email address.');
+      } else if (response.status === 404) {
+        alert("User not found!")        
+      }
+
+      const data = await response.text();
+      // Handle successful login
+      console.log(data);
+    } catch (error) {
+      // Handle network errors or other issues
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className='Login'>
       {showSignup ? (
@@ -40,7 +78,7 @@ export default function Login() {
                 Don't have an account yet?{' '}
                 <span className='Signup-now' onClick={handleSignUpClick}>Sign up now</span>
               </p>
-              <form>
+              <form onSubmit={handleLogin}>
                 <div className="form-group">
                   <input type="text" className="losign" name="email" placeholder="Email Address" />
                 </div>
