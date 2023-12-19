662e414ae9ccdf8fb0fde907427fdd2f67f5981b
kerem <keremozrtk@gmail.com>
2023-12-19 11:34:43 +0300

    Log in component v1

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Login from './Login'; // Assuming Login component is in a separate file
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
   return (
     <div className='header'>
       <div className='Header-logo-container'>
         <a href='/' className='Header-logo'>SemantiQ</a>
       </div>
       <div className='Header-button-container'>
-        <button className='Header-login-button'>Log in</button>
+        <button className='Header-login-button' onClick={handleLoginClick}>Log in</button>
         <button className='Header-signup-button'>Sign up</button>
       </div>
+      {showLogin && <Login />}
     </div>
-  )
+  );
 }
---
+++ app/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className='Login'>
+      <div className='Login-box'>
+        <div className='Login-pattern'></div>
+        <div className='Login-base'>
+          <div className="login-container">
+            <h2 className='Login-heading'>Welcome!</h2>
+            <p className='Login-text'>
+              Don't have an account yet? <span className='Signup-now'>Sign up now</span>
+            </p>
+            <form>
+              <div className="form-group">
+                <input type="text" id="email" name="email" placeholder="Email Address" />
+              </div>
+              <div className="form-group">
+                <input type="password" id="password" name="password" placeholder="Password" />
+              </div>
+              <div className="form-group">
+                <input type="checkbox" id="rememberMe" name="rememberMe" />
+                <label className='Login-label' htmlFor="rememberMe">Remember Me</label>
+              </div>
+              <div className="form-group">
+                <input id="Login-submit" type="submit" value="Sign in" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
